fd28e43ca73261ef44cfd60db24e1001a67c71d8
Clauthucio Chaves <clauthucio@gmail.com>
2025-06-30 18:46:47 -0400

    Inclusão de mais duas validações

--- src/test/java/petstoreswagger/PetStoreTest.java
+++ src/test/java/petstoreswagger/PetStoreTest.java
@@ -1,17 +1,21 @@
 package petstoreswagger;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
-
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PetStoreTest {
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.baseURI = "https://petstore.swagger.io/v2";
+    }
+
     @Test
     public void testAdicionarPet(){
-        baseURI = "https://petstore.swagger.io/v2";
         given()
             .contentType(ContentType.JSON)
             .body("{\n" +
@@ -42,7 +46,6 @@ public class PetStoreTest {
 
     @Test
     public void testEncontrarPorStatus() {
-        baseURI = "https://petstore.swagger.io/v2";
         String status = "available";
         given()
             .queryParam("status", status)
@@ -56,7 +59,7 @@ public class PetStoreTest {
 
     @Test
     public void testRemoverPet(){
-        baseURI = "https://petstore.swagger.io/v2";
+        // Cria um pet para depois remover
         long petID = given()
             .contentType(ContentType.JSON)
             .body("{\n" +
@@ -92,5 +95,57 @@ public class PetStoreTest {
             .assertThat()
                 .statusCode(200)
                 .body("message", equalTo(String.valueOf(petID)));
+    }
+
+    @Test
+    public void testEfetuarPedido(){
+        given()
+            .contentType(ContentType.JSON)
+            .body("{\n" +
+                    "  \"id\": 0,\n" +
+                    "  \"petId\": 0,\n" +
+                    "  \"quantity\": 0,\n" +
+                    "  \"shipDate\": \"2025-06-30T22:04:14.473Z\",\n" +
+                    "  \"status\": \"placed\",\n" +
+                    "  \"complete\": true\n" +
+                    "}")
+        .when()
+            .post("/store/order")
+        .then()
+            .assertThat()
+                .statusCode(200)
+                .body("id", not(empty()));
+    }
+
+    @Test
+    public void testRemoverPedido(){
+        // Cria um pedido para depois deletar
+        long orderID = given()
+            .contentType(ContentType.JSON)
+            .body("{\n" +
+                    "  \"id\": 0,\n" +
+                    "  \"petId\": 0,\n" +
+                    "  \"quantity\": 0,\n" +
+                    "  \"shipDate\": \"2025-06-30T22:04:14.473Z\",\n" +
+                    "  \"status\": \"placed\",\n" +
+                    "  \"complete\": true\n" +
+                    "}")
+        .when()
+            .post("/store/order")
+        .then()
+            .assertThat()
+                .statusCode(200)
+                .extract()
+                    .path("id");
+
+        // Agora deleta o pedido criado
+        given()
+            .pathParam("orderId", orderID)
+        .when()
+            .delete("/store/order/{orderId}")
+        .then()
+            .assertThat()
+                .statusCode(200)
+                .body("message", equalTo(String.valueOf(orderID)));
     }
 }
